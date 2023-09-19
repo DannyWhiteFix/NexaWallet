c782b2d27946597081913e154770c5602dee6a55
Cory Fields <cory-nospam-@coryfields.com>
2023-09-19 16:13:26 +0200

    depends: specify libc++ header location for darwin
    For depends builds this was fixed by fbcfcf69, which deleted the conflicting
    eaders. When we no longer control the clang installation, we need to ensure
    that the SDK's libc++ headers are used rather than the ones shipped with clang.
    We can do that by turning off the default include path and hard-coding our own.
    This hard-coded path is ok because we control (via SDK packaging) where these
    headers end-up.

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -2,8 +2,8 @@ OSX_MIN_VERSION=10.14
 OSX_SDK_VERSION=10.15
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=609
-darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
-darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -stdlib=libc++ -mlinker-version=$(LD64_VERSION)
+darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) -isysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -B$(build_prefix)/bin
+darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) -isysroot $(OSX_SDK) -stdlib=libc++ -mlinker-version=$(LD64_VERSION) -B$(build_prefix)/bin -nostdinc++ -isystem $(OSX_SDK)/usr/include/c++/v1
 
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
