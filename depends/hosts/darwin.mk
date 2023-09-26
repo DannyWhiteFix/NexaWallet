be037b3449f4ba65fafd38a83e7647e2e8b7c0b3
Carl Dong <contact@carldong.me>
2023-09-26 09:53:14 +0200

    depends: Fully determine path for darwin_{CC,CXX}
    Instead of doing the awkward /bin path prepending at config.site
    creation time, set darwin_{CC,CXX} in a way that fully determines the
    program's path (clang/clang++) similar to how AC_PATH_{TOOL,PROG} would
    do.
    
    Also see the added comment block in depends/Makefile for more context on
    determining $PATH for our config.site.

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -2,9 +2,6 @@ OSX_MIN_VERSION=11.0
 OSX_SDK_VERSION=11.3
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=609
-darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) -isysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -B$(build_prefix)/bin
-darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) -isysroot $(OSX_SDK) -stdlib=libc++ -mlinker-version=$(LD64_VERSION) -B$(build_prefix)/bin -nostdinc++ -isystem $(OSX_SDK)/usr/include/c++/v1
-
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
 
@@ -15,3 +12,10 @@ darwin_debug_CFLAGS=-O1
 darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
 
 darwin_native_toolchain=native_cctools
+clang_prog=$(build_prefix)/bin/clang
+clangxx_prog=$(clang_prog)++
+llvm_config_prog=$(build_prefix)/bin/llvm-config
+
+darwin_CC=$(clang_prog) -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) -isysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -B$(build_prefix)/bin
+
+darwin_CXX=$(clangxx_prog) -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) -isysroot $(OSX_SDK) -stdlib=libc++ -mlinker-version=$(LD64_VERSION) -B$(build_prefix)/bin -nostdinc++ -isystem $(OSX_SDK)/usr/include/c++/v1
