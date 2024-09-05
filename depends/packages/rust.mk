e683caf7ab615443accca951c93dea5d2c017d11
Andrea Suisani <sickpig@gmail.com>
2024-09-05 15:53:30 +0200

    [build] Update rust package version in the depends tree
    According to https://bitcoinunlimited.gitlab.io/rostrum/build/ the
    rust min version to compile rostrum is 1.74.0, hence I updated the
    current version 1.73.0 to the latest stable version 1.80.1
    
    Add rust to the list of dependencies liste in doc/dependencies.md

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,8 @@
 package=rust
-$(package)_version=1.73.0
+$(package)_version=1.80.1
 $(package)_download_path=https://static.rust-lang.org/dist
 $(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash=aa4cf0b7e66a9f5b7c623d4b340bb1ac2864a5f2c2b981f39f796245dc84f2cb
+$(package)_sha256_hash=9eedaea9719914a0f4673899aa11693607f25486569913fcca5905ef1da784ec
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
