e6bf3634490e4c3fd6b84ba56f2769b2a4eaf17b
Andrea Suisani <sickpig@gmail.com>
2026-01-14 14:27:43 +0000

    Use rostrum master branch in CI

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,8 @@
 package=rust
-$(package)_version=1.86.0
+$(package)_version=1.91.1
 $(package)_download_path=https://static.rust-lang.org/dist
 $(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash="f6a8c0d8b8a8a737c40eee78abe286a3cbe984d96b63de9ae83443360e3264bf"
+$(package)_sha256_hash="1c955c040dd087e4751d15588ddec288b4208bea16f8ec5046c164877e55fff7"
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
